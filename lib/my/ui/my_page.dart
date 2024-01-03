4f2687c85e00355bc30b5cab3d324a1e20a3ec51
markliu2013 <markliu2013@gmail.com>
2024-01-04 00:02:35 +0800

    save1

--- lib/my/ui/my_page.dart
+++ lib/my/ui/my_page.dart
@@ -103,7 +103,7 @@ class MyPage extends StatelessWidget {
               const Divider(),
               const ListTile(
                   title: Text('当前版本号：'),
-                  trailing: Text('1.0.20')
+                  trailing: Text('1.0.21')
               ),
               const Divider(),
               Container(
