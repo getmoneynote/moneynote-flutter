e1e22e6dda4d8f230dcf96e5aebe4a1367f75393
anonymous <anonymous@1.com>
2024-03-21 20:03:15 +0800

    save

--- lib/app/core/values/app_values.dart
+++ lib/app/core/values/app_values.dart
@@ -1,7 +1,7 @@
 class AppValues {
 
   static const String appName = 'MoneyNote';
-  static const String version = '1.0.40';
+  static const String version = '1.0.41';
   static String apiUrl = '';
 
 }
